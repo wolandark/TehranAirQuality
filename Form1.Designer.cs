169e2ab55dbd5b7f2292ba5348407c35be8b40b2
wolandark <107309764+wolandark@users.noreply.github.com>
2024-12-12 23:43:36 -1100

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace AirQuality
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.button1 = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Highlight;
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(1, 0);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.Size = new System.Drawing.Size(247, 225);
+      this.dataGridView1.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.SystemColors.Info;
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+      this.button1.Location = new System.Drawing.Point(1, 219);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(247, 59);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Update";
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.button1_Click_1);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.BackgroundImage = global::AirQuality.Properties.Resources.mask_icon_140145;
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+      this.ClientSize = new System.Drawing.Size(246, 275);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.dataGridView1);
+      this.DoubleBuffered = true;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximumSize = new System.Drawing.Size(262, 314);
+      this.MinimumSize = new System.Drawing.Size(262, 314);
+      this.Name = "Form1";
+      this.Text = "Air Quality";
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.DataGridView dataGridView1;
+    private System.Windows.Forms.Button button1;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace AirQuality
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      dataGridView1.Visible = false;
+      FetchAirQualityData();
+    }
+
+    private async void FetchAirQualityData()
+    {
+      try
+      {
+        // API URL
+        string apiUrl = "https://api.waqi.info/feed/tehran/?token=5d5b2e0d0c07cff6625c8ecaa781d38cc831c333";
+
+        using (HttpClient client = new HttpClient())
+        {
+          // Fetch data
+          HttpResponseMessage response = await client.GetAsync(apiUrl);
+          response.EnsureSuccessStatusCode();
+
+          string jsonData = await response.Content.ReadAsStringAsync();
+
+          // Parse JSON
+          var airQualityData = JsonConvert.DeserializeObject<AirQualityResponse>(jsonData);
+
+          // Display data in DataGridView
+          DisplayData(airQualityData);
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show($"Error: {ex.Message}");
+      }
+    }
+
+    //private void DisplayData(AirQualityResponse data)
+    //{
+    //  if (data.Status == "ok")
+    //  {
+    //    // Add rows to DataGridView
+    //    dataGridView1.Columns.Add("Parameter", "Parameter");
+    //    dataGridView1.Columns.Add("Value", "Value");
+
+    //    // Add key metrics to table
+    //    dataGridView1.Rows.Add("AQI", data.Data.Aqi);
+    //    dataGridView1.Rows.Add("Dominant Pollutant", data.Data.Dominentpol);
+    //    dataGridView1.Rows.Add("PM2.5", data.Data.Iaqi.Pm25?.V);
+    //    dataGridView1.Rows.Add("PM10", data.Data.Iaqi.Pm10?.V);
+    //    dataGridView1.Rows.Add("NO2", data.Data.Iaqi.No2?.V);
+    //    dataGridView1.Rows.Add("SO2", data.Data.Iaqi.So2?.V);
+    //    dataGridView1.Rows.Add("Temperature", data.Data.Iaqi.T?.V);
+    //    dataGridView1.Rows.Add("Humidity", data.Data.Iaqi.H?.V);
+    //  }
+    //  else
+    //  {
+    //    MessageBox.Show("Failed to fetch air quality data.");
+    //  }
+    //}
+
+    private void DisplayData(AirQualityResponse data)
+    {
+      if (data.Status == "ok")
+      {
+        dataGridView1.RowHeadersVisible = false;
+        dataGridView1.ScrollBars = ScrollBars.None;
+        dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        // Clear existing columns and rows
+        dataGridView1.Columns.Clear();
+        dataGridView1.Rows.Clear();
+
+        // Recreate columns
+        dataGridView1.Columns.Add("Parameter", "Parameter");
+        dataGridView1.Columns.Add("Value", "Value");
+
+        // Add key metrics to table
+        dataGridView1.Rows.Add("AQI", data.Data.Aqi);
+        dataGridView1.Rows.Add("Dominant Pollutant", data.Data.Dominentpol);
+        dataGridView1.Rows.Add("PM2.5", data.Data.Iaqi.Pm25?.V);
+        dataGridView1.Rows.Add("PM10", data.Data.Iaqi.Pm10?.V);
+        dataGridView1.Rows.Add("NO2", data.Data.Iaqi.No2?.V);
+        dataGridView1.Rows.Add("SO2", data.Data.Iaqi.So2?.V);
+        dataGridView1.Rows.Add("Temperature", data.Data.Iaqi.T?.V);
+        dataGridView1.Rows.Add("Humidity", data.Data.Iaqi.H?.V);
+
+        dataGridView1.Visible = true;
+
+      }
+      else
+      {
+        MessageBox.Show("Failed to fetch air quality data.");
+      }
+    }
+
+    private void button1_Click_1(object sender, EventArgs e)
+    {
+      FetchAirQualityData();
+    }
+  }
+
+  // Classes to parse JSON response
+  public class AirQualityResponse
+  {
+    public string Status { get; set; }
+    public AirQualityData Data { get; set; }
+  }
+
+  public class AirQualityData
+  {
+    public int Aqi { get; set; }
+    public string Dominentpol { get; set; }
+    public Iaqi Iaqi { get; set; }
+  }
+
+  public class Iaqi
+  {
+    public Value Pm25 { get; set; }
+    public Value Pm10 { get; set; }
+    public Value No2 { get; set; }
+    public Value So2 { get; set; }
+    public Value T { get; set; }
+    public Value H { get; set; }
+  }
+
+  public class Value
+  {
+    public double? V { get; set; }
+  }
+}
